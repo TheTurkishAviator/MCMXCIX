3738727c7af33a9cc2519bae667c6a4a6aca2d10
Vic Rattlehead <167060963+TheTurkishAviator@users.noreply.github.com>
2025-05-30 14:54:46 +0300

    Add files via upload

---
+++ mcmxcix/level/GlitchControl.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class GlitchControl : ColorRect
+{
+    [Export] public float MaxGlitchStrength = 0.05f;
+    [Export] public float GlitchInterval = 0.05f;
+    private ShaderMaterial _material;
+    private float _timer;
+
+    public override void _Ready()
+    {
+        _material = (ShaderMaterial)Material;
+    }
+    public override void _Process(double delta)
+    {
+        _timer += (float)delta;
+        if (_timer > GlitchInterval)
+        {
+            var rnd = new RandomNumberGenerator();
+            rnd.Randomize();
+
+            _material.SetShaderParameter("rbg_split_amount", rnd.Randf() * MaxGlitchStrength);
+            _material.SetShaderParameter("distortion_strength", rnd.Randf() * MaxGlitchStrength);
+            _timer = 0;
+        }
+    }
+}
